64e25b231928557cccef3b65581db1bd4a2635b1
Christopher Brown <xoebus@xoeb.us>
2017-03-20 11:58:28 -0700

    [tlsconfig] steer clear of elliptic curve

--- secure/tlsconfig/config.go
+++ secure/tlsconfig/config.go
@@ -77,9 +77,7 @@ func WithPivotalDefaults() TLSOption {
 		c.MinVersion = tls.VersionTLS12
 		c.PreferServerCipherSuites = true
 		c.CipherSuites = []uint16{
-			tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
 			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
-			tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
 			tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
 		}
 		c.CurvePreferences = []tls.CurveID{
